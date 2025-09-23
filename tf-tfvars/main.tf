01ee401af88470e8916aeb03b0c29fd74fe004c4
Üba3ba <166398925+Alphanumerisch@users.noreply.github.com>
2025-09-23 08:32:25 +0200

    OK

---
+++ tf-tfvars/main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        local = {source = "hashicorp/local", version = "> 2.4.0"}
+    }
+}
+
+resource "local_file" "cfg" {
+    filename = "${path.module/cfg_${var.env}_${var.name}.ini}"
+    content = <<EOT
+name = ${var.name}
+env = ${var.env}
+num = ${var.number}
+EOT
+}
+
+output "config_file" {
+    value = local_file.cfg.filename
+}
---
+++ tf-tfvars/variables.tf
@@ -0,0 +1,3 @@
+variable "name" {type = string}
+variable "env" {type = string, default = "dev"}
+variable "number" {type = number, default = 1}
